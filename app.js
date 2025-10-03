cdcb7271b702c88a52f1f20546d0360481fe3d16
Abdulrafay527 <rafaymoosait@gmail.com>
2025-10-03 15:50:42 +0500

    switch statement

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+var day = prompt("Enter your day");
+
+switch(day){
+    case "tue":
+        console.log("Theory");
+        break
+    case "thu":
+        console.log("Theory");
+        break
+    case "sat":
+        console.log("Practical");
+        break
+    default :
+        console.log("Kuch nhi");               
+}
