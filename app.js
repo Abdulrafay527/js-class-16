cba4a3c6e7e8beab00288f8fdd5cc4b4914b9744
Abdulrafay527 <rafaymoosait@gmail.com>
2025-10-06 15:31:50 +0500

    dom practice

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 
 
-var day = prompt("Enter your day");
+// var day = prompt("Enter your day");
 
-switch(day){
-    case "tue":
-        console.log("Theory");
-        break
-    case "thu":
-        console.log("Theory");
-        break
-    case "sat":
-        console.log("Practical");
-        break
-    default :
-        console.log("Kuch nhi");               
-}
+// switch(day){
+//     case "tue":
+//         console.log("Theory");
+//         break
+//     case "thu":
+//         console.log("Theory");
+//         break
+//     case "sat":
+//         console.log("Practical");
+//         break
+//     default :
+//         console.log("Kuch nhi");               
+// }
+
+
+// var div = document.getElementById("main")
+// div.innerHTML = "<h1>rafay moosa </h1>";
+
+// console.log(div.getAttribute("class"))
+
+
+// var input = document.getElementById("text-input");
+
+// input.value = "rafay moosa"
+
+// function submit(){
+//     console.log(input.value)
+// }
